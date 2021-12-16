b9e6106e7c278b1d06ada1b31fb1c71b167fbd9d
방순현 <0189875@naver.com>
2021-12-16 14:50:13 +0900

    refresh

--- Android Target SDK3 DeviceID Update/Device Update.sql
+++
@@ -1,6 +0,0 @@
---USE MOBILE_SERVICE
-
-UPDATE MOBILE_PUSH_DEVICE_INFO
-SET DeviceID = DeviceNewID
-WHERE DeviceNewID IS NOT NULL  AND DeviceNewID <> '' AND DeviceID <> DeviceNewID
-AND DeviceType = 'android'
