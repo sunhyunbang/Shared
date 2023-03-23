029f9b17f65d758ec8a9ea1fa7134965e4a9a6ac
snhyun82 <0189875@naver.com>
2023-03-23 13:09:52 +0900

    11

---
+++ vue/.prettierrc.js
@@ -0,0 +1,8 @@
+{
+  "semi": false, // 마지막에 세미콜론 여부
+  "singleQuote": true, // 문자열 작은 따옴표
+  "printWidth": 80, // 한 줄에 80글자
+  "useTabs": false, // 띄어쓰기 대신 탭 사용 x
+  "trailingComma": "none",  // object 마지막에 콤마 여부
+  "bracketSpacing": true // object 괄호 공백
+}
