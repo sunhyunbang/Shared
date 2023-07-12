4463518e083f84f041e9113830d1f4128ef62aaf
snhyun82 <0189875@naver.com>
2023-07-12 14:58:42 +0900

    22

--- vue/addrow/addRowView3.vue
+++ vue/addrow/addRowView3.vue
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <title>Vue 3 App</title>
-  <script src="https://unpkg.com/vue@next"></script>
-  <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/accounting@latest/accounting.min.js"></script>
-</head>
-
-<body>
-  <div id="app">
+<template>
+  <div class="panel-body" id="app">
     <table class="table table-hover">
       <thead>
         <tr>
@@ -23,145 +12,137 @@
           <th style="width: 130px;"></th>
         </tr>
       </thead>
-      <tbody v-sortable:rows="rows">
+      <tbody v-sortable:draggable="rows">
         <tr v-for="(row, index) in rows" :key="index">
           <td>{{ index + 1 }}</td>
-          <td><input class="form-control" v-model="row.description" /></td>
-          <td><input class="form-control" v-model.number="row.qty" /></td>
           <td>
-            <input class="form-control text-right" v-model.number="row.price" @input="calculateTotal(row)" />
+            <input class="form-control" v-model="row.description" />
           </td>
           <td>
-            <select class="form-control" v-model.number="row.tax" @change="calculateTotal(row)">
+            <input class="form-control" v-model="row.qty" type="number" />
+          </td>
+          <td>
+            <input
+              class="form-control text-right"
+              v-model="row.price"
+              type="number"
+              step="0.01"
+            />
+          </td>
+          <td>
+            <select class="form-control" v-model="row.tax">
               <option value="0">0%</option>
               <option value="10">10%</option>
               <option value="20">20%</option>
             </select>
           </td>
           <td>
-            <input class="form-control text-right" :value="calculateRowTotal(row)" readonly />
-            <input type="hidden" :value="calculateTaxAmount(row)" />
+            <input
+              class="form-control text-right"
+              :value="row.qty * row.price"
+              readonly
+            />
+            <input
+              type="hidden"
+              :value="(row.qty * row.price * row.tax) / 100"
+            />
           </td>
           <td>
-            <button class="btn btn-primary btn-xs" @click="addRow(index)">Add Row</button>
-            <button class="btn btn-danger btn-xs" @click="removeRow(index)">Remove Row</button>
+            <button class="btn btn-primary btn-xs" @click="addRow(index)">
+              add row
+            </button>
+            <button class="btn btn-danger btn-xs" @click="removeRow(index)">
+              remove row
+            </button>
           </td>
         </tr>
       </tbody>
       <tfoot>
         <tr>
           <td colspan="5" class="text-right">TAX</td>
-          <td colspan="1" class="text-right">{{ taxTotal }}</td>
+          <td colspan="1" class="text-right">{{ computedTaxTotal }}</td>
           <td></td>
         </tr>
         <tr>
           <td colspan="5" class="text-right">TOTAL</td>
-          <td colspan="1" class="text-right">{{ total }}</td>
+          <td colspan="1" class="text-right">{{ computedTotal }}</td>
           <td></td>
         </tr>
         <tr>
           <td colspan="5" class="text-right">DELIVERY</td>
           <td colspan="1" class="text-right">
-            <input class="form-control text-right" v-model.number="delivery" />
+            <input class="form-control text-right" v-model="delivery" type="number" />
           </td>
           <td></td>
         </tr>
         <tr>
           <td colspan="5" class="text-right"><strong>GRANDTOTAL</strong></td>
-          <td colspan="1" class="text-right"><strong>{{ grandTotal }}</strong></td>
+          <td colspan="1" class="text-right">
+            <strong>{{ computedTotal + delivery }}</strong>
+          </td>
           <td></td>
         </tr>
       </tfoot>
     </table>
     <button @click="getData()">SUBMIT DATA</button>
-    <pre>{{ $data | json }}</pre>
+    <pre>{{ JSON.stringify($data) }}</pre>
   </div>
+</template>
 
-  <script>
-    const { createApp, ref } = Vue;
+<script setup>
+import { ref } from 'vue';
+import Sortable from 'sortablejs';
 
-    createApp({
-      setup() {
-        const rows = ref([
-          { qty: 5, description: "Something", price: 55.20, tax: 10 },
-          { qty: 2, description: "Something else", price: 1255.20, tax: 20 },
-        ]);
+const rows = ref([
+  { qty: 5, description: 'Something', price: 55.20, tax: 10 },
+  { qty: 2, description: 'Something else', price: 1255.20, tax: 20 },
+]);
+const total = ref(0);
+const grandtotal = ref(0);
+const taxtotal = ref(0);
+const delivery = ref(40);
 
-        const total = ref(0);
-        const grandTotal = ref(0);
-        const taxTotal = ref(0);
-        const delivery = ref(40);
+const computedTotal = computed(() => {
+  return rows.value.reduce((acc, row) => acc + row.total, 0);
+});
 
-        const addRow = (index) => {
-          rows.splice(index + 1, 0, {});
-        };
+const computedTaxTotal = computed(() => {
+  return rows.value.reduce((acc, row) => acc + row.tax_amount, 0);
+});
 
-        const removeRow = (index) => {
-          rows.splice(index, 1);
-        };
+const addRow = (index) => {
+  rows.value.splice(index + 1, 0, {});
+};
 
-        const calculateRowTotal = (row) => {
-          const total = row.qty * row.price;
-          const taxAmount = total * row.tax / 100;
-          return accounting.formatMoney(total, "$", 2, ".", ",");
-        };
+const removeRow = (index) => {
+  rows.value.splice(index, 1);
+};
 
-        const calculateTaxAmount = (row) => {
-          const total = row.qty * row.price;
-          return accounting.formatMoney(total * row.tax / 100, "$", 2, ".", ",");
-        };
+const getData = () => {
+  fetch('/api/data', {
+    method: 'POST',
+    body: JSON.stringify({
+      rows: rows.value,
+      total: total.value,
+      delivery: delivery.value,
+      taxtotal: taxtotal.value,
+      grandtotal: grandtotal.value,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
 
-        const calculateTotal = (row) => {
-          total.value = rows.reduce((sum, current) => {
-            return sum + current.qty * current.price;
-          }, 0);
-          taxTotal.value = rows.reduce((sum, current) => {
-            return sum + (current.qty * current.price * current.tax) / 100;
-          }, 0);
-          grandTotal.value = total.value + delivery.value;
-        };
+const sortableOptions = {
+  draggable: 'tr',
+};
 
-        const getData = () => {
-          const formattedRows = rows.map((row) => {
-            return {
-              qty: row.qty,
-              description: row.description,
-              price: row.price,
-              tax: row.tax,
-              total: row.qty * row.price,
-              taxAmount: row.qty * row.price * row.tax / 100,
-            };
-          });
-
-          const data = {
-            rows: formattedRows,
-            total: total.value,
-            delivery: delivery.value,
-            taxTotal: taxTotal.value,
-            grandTotal: grandTotal.value,
-          };
-
-          console.log(data);
-
-          // Send the data to the API using AJAX
-        };
-
-        return {
-          rows,
-          total,
-          grandTotal,
-          taxTotal,
-          delivery,
-          addRow,
-          removeRow,
-          calculateRowTotal,
-          calculateTaxAmount,
-          calculateTotal,
-          getData,
-        };
-      },
-    }).mount("#app");
-  </script>
-</body>
-
-</html>
+onMounted(() => {
+  const sortable = Sortable.create($refs.table, sortableOptions);
+  sortable.option('onUpdate', (e) => {
+    const item = rows.value.splice(e.oldIndex, 1)[0];
+    rows.value.splice(e.newIndex, 0, item);
+  });
+});
+</script>
